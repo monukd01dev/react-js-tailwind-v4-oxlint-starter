bb78fc0564043d2bbbc32fc0aa6ebb90a99bad4c
MonuKD-2026 <monukd01dev@gmail.com>
2026-06-28 10:45:30 +0530

    started-test-done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold bg-lime-400">
+        Tailwind v4 and React JS starter
+      </h1>
+    </>
+  )
+}
+
+export default App
